5b325f372a2399fecc029873a73d9b7aa9c0033c
Stranger N <91267135+adventurboy@users.noreply.github.com>
2021-10-14 15:58:05 +0400

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -53,7 +53,8 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**🗂️ Title:** {search}\n**🎞 Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**    📤 Uploaded by {message.chat.title}**"
+        mo_tech_yt = f"**🗂️ Title:** {search}\n**🎞 Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**
+        © Uploaded by {message.chat.title}**"
         files = await get_filter_results(query=search)
         if files:
             for file in files:
@@ -115,7 +116,8 @@ async def group(client, message):
     if 2 < len(message.text) < 50:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**🗂️ Title:** {search}\n**✈ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**        📤 Uploaded by {message.chat.title}**"
+        mo_tech_yt = f"**🗂️ Title:** {search}\n**✈ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**
+        © Uploaded by {message.chat.title}**"
         nyva=BOT.get("username")
         if not nyva:
             botusername=await client.get_me()
