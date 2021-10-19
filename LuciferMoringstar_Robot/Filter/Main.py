42dce35b4ca19fcfd1437bfcbcf19428f449f26e
Stranger N <91267135+adventurboy@users.noreply.github.com>
2021-10-19 11:20:23 +0400

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -290,7 +290,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "help":
             buttons = [[
                 InlineKeyboardButton('Update Channel', url='t.me/movie_fanda_official'),
-                InlineKeyboardButton('Source Code', url="https://t.me/AdhavaaBiriyaniKittiyalo")
+                InlineKeyboardButton('Source Code', url="t.me/AdhavaaBiriyaniKittiyalo")
                 ],[
                 InlineKeyboardButton('💫Deploy Video💫', url=f'{TUTORIAL}')
                 ]]
@@ -300,7 +300,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             buttons = [
                 [
                     InlineKeyboardButton('Update Channel', url='t.me/movie_fanda_official'),
-                    InlineKeyboardButton('Source Code', url='https://t.me/AdhavaaBiriyaniKittiyalo)
+                    InlineKeyboardButton('Source Code', url='t.me/AdhavaaBiriyaniKittiyalo)
                 ]
                 ]
             await query.message.edit(text=f"{ABOUT}".format(TUTORIAL), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
